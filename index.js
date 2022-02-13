62190f9d7a39a4376eb32853ba317c5cde00b103
Thomas James <tomsterlingjames@outlook.com.au>
2022-02-13 16:52:56 +1000

    Packages added and questions added.

--- index.js
+++ index.js
@@ -9,7 +9,48 @@ const questions = [
         name: 'Title',
         message: 'What is your repository title?'
     },
-    
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'What is your repository description?'
+    },
+    {
+        type: 'input',
+        name: 'Install',
+        message: 'write your instructions on how to install.'
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'Write your usage section here.'
+    },
+    {
+        type: 'input',
+        name: 'Contribute',
+        message: 'How can others contribute to your repo?'
+    },
+    {
+        type: 'input',
+        name: 'Test',
+        message: 'How can others test your application?'
+    },
+    {
+        type: 'list',
+        name: 'Licensing',
+        message: 'Select a license.',
+        choices: ['Apache License 2.0', 'MIT License', 'Mozilla Public License 2.0',
+            'GNU GLPv3', 'GNU LGPLv3', 'GNU AGPLv3', 'Boost Software License 1.0', 'The Unlicense'],
+    },
+    {
+        type: 'input',
+        name: 'userName',
+        message: 'Enter your Github user name.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        Message: 'Enter your email address.',
+},
 ];
 
 // TODO: Create a function to write README file
