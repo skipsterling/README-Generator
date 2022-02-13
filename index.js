5ec5371fcc9d7ba89af36c1b47eac6d337da8a72
Thomas James <tomsterlingjames@outlook.com.au>
2022-02-13 16:42:25 +1000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const RMGen = require('./utils/generateMarkdown');
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is your repository title?'
+    },
+    
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
