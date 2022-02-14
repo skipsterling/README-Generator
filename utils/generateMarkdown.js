b79c12f1f6e8b26155ca08de23c51839cb0adf70
Thomas James <tomsterlingjames@outlook.com.au>
2022-02-14 20:55:11 +1000

    License link function done

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -33,6 +33,37 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
+function renderLicenseLink(license) {
+  let licenceLink;
+  switch (license) {
+    case 'Apache License 2.0':
+      licenceLink = '[![License: Apache 2.0](https://choosealicense.com/licenses/apache-2.0/)';
+      break;
+      case 'MIT License':
+      licenceLink = '[![License: MIT](https://choosealicense.com/licenses/mit/)';
+      break;
+      case 'Mozilla Public License 2.0':
+      licenceLink = '[![License: Mozilla 2.0](https://choosealicense.com/licenses/mpl-2.0/)';
+      break;
+      case 'GNU GPLv3':
+        licenceLink = '[![License: GPLv3](https://choosealicense.com/licenses/gpl-3.0/)';
+        break;
+        case 'GNU LGPLv3':
+      licenceLink = '[![License: LGPLv3](https://choosealicense.com/licenses/lgpl-3.0/)';
+      break;
+      case 'Boost Software License 1.0':
+        licenceLink = '[![License: Boost 1.0](https://choosealicense.com/licenses/bsl-1.0/)';
+        break;
+        case 'The Unlicense':
+      licenceLink = '[![License: The Unlicense](https://choosealicense.com/licenses/unlicense/)';
+      break;
+      case 'License not required':
+        licenceLink = '';
+        break;
+  }
+  return licenceLink;
+}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
